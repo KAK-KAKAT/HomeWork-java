3f94a8bff7b71c38605ba80a4779fdd24160d0e6
Blebik <ofnk112@gmail.com>
2022-09-18 23:22:54 +0300

    Add Homework

---
+++ HW_003/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] Task;
+        Task = new int[10];
+        int a;
+        a = 0;
+        System.out.print("Answer: ");
+        for (int i = 0; i < 10; i++) {
+            Task[i] = i;
+            if (i % 2 == 1) {
+                a += i;
+            }
+            System.out.print(a + " ");
+        }
+    }
+}
